1dbfc75b86fc6237446dac17c466d54c8a0734e2
Gregory Delpe <gregory@namely.com>
2015-05-24 19:23:02 -0400

    Parses pull request id from json data dump

--- main.py
+++ main.py
@@ -1,2 +1,20 @@
-print "Hello World"
-I
+import json
+import re
+from pprint import pprint
+
+def pr_number_from(url):
+  matcher = re.compile(".*?/pr-(?P<pr_number>\d{4})")
+  full_url = url['Names'][0]
+  pr_number = matcher.match(full_url)
+  if pr_number:
+    pr_number = pr_number.group('pr_number')
+    return pr_number
+
+if __name__ == '__main__':
+  with open('ansible_dump.json') as data_file:
+    data = json.load(data_file)
+    pr_numbers = map(pr_number_from, data)
+    pr_numbers = filter(lambda value: value is not None, pr_numbers)
+    pprint(pr_numbers)
+
+  pprint(len(data))
